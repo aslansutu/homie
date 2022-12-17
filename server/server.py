32b78b0aa7bd213445f92661a1dc7a24139cae40
Atakan Onol <atakan16@gmail.com>
2022-12-17 19:59:00 +0000

    Updated port

--- server/server.py
+++ server/server.py
@@ -61,4 +61,4 @@ async def is_up(ip: str):
     return {"IP Status:": scanner[host].state()}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
+    uvicorn.run(app, host="0.0.0.0", port=80, log_level="info")
